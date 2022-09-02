e0d673416d7e5e3516895e6d619442ca28f33d91
Vivek Lakade <83109943+itsVivekLakade@users.noreply.github.com>
2022-09-02 14:27:24 +0530

    Create Assignment2.py

---
+++ Assignment2.py
@@ -0,0 +1,20 @@
+from jenkinsapi.jenkins import Jenkins
+import requests
+
+myUrl = "http://localhost:8080/"
+myUsername = "vila274183"
+myPassword = "111"
+
+jnk = Jenkins(myUrl, myUsername, myPassword)
+totalBuildCount = 0
+
+for jobs in jnk.keys():
+    builds = jnk[jobs].get_build_dict()
+    print (jobs + " = build count: " + str(len(builds)))
+    # totalBuildCount += len(builds)
+    print (jobs)
+    jnk.build_job(jobs)
+
+# print ("Total build count: " + str(totalBuildCount))
+job = "test"
+jnk.build_job(job)
