b1fb2aceb25e9e57ef67f11c526bea34bd1dc048
Vivek Lakade <83109943+itsVivekLakade@users.noreply.github.com>
2022-09-06 14:40:00 +0530

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -1,9 +1,8 @@
 from jenkinsapi.jenkins import Jenkins
-import requests
 
 myUrl = "http://localhost:8080/"
 myUsername = "vila274183"
-myPassword = "111"
+myPassword = "110a3997288372c6645b6bb24676bf4ad7"
 
 jnk = Jenkins(myUrl, myUsername, myPassword)
 totalBuildCount = 0
@@ -12,8 +11,8 @@ for jobs in jnk.keys():
     builds = jnk[jobs].get_build_dict()
     print (jobs + " = build count: " + str(len(builds)))
     # totalBuildCount += len(builds)
-    print (jobs)
-    jnk.build_job(jobs)
+    #print (jobs)
+    #jnk.build_job(jobs,'')
 
 # print ("Total build count: " + str(totalBuildCount))
 job = "test"
